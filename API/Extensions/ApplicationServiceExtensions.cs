1748fff7befbc493874770c864adcb3b4890675c
hylosp@icloud.com <hylosp@icloud.com>
2023-01-24 23:24:58 +0200

    Created CRUD Operations in the API

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection Services, IConfiguration config)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            Services.AddEndpointsApiExplorer();
+            Services.AddSwaggerGen();
+
+            //DbContext
+            Services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+
+            });
+
+            //Add Cors Policy
+            Services.AddCors(opt => {
+
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            //add mediator
+            Services.AddMediatR(typeof(List.Handler));
+            //Add automapper
+            Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return Services;
+
+        }
+    }
+}
