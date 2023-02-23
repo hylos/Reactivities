f29772d292002bbd5be9d29a9167f8f072cd1b10
hylosp@icloud.com <hylosp@icloud.com>
2023-02-23 22:05:35 +0200

    Created image upload on client-app, with the capabilities of setting/changing the main profile picture and also delete photos on their profile.

---
+++ client-app/src/app/common/imageUpload/PhotoWidgetDropzone.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone';
+import { Header, Icon } from 'semantic-ui-react';
+// import {useDropzone} from 'react-dropzone'
+
+interface Props {
+  setFiles: (files: any) => void;
+}
+
+export default function PhotoUploadWidget({ setFiles }: Props) {
+  const dzSytles = {
+    border: 'dashed 3px #eee',
+    borderColor: '#eee',
+    borderRadius: '5px',
+    paddingTop: '30px',
+    textAlign: 'center' as 'center',
+    height: 200
+  }
+
+  const dzActive = {
+    borderColor: 'green'
+  }
+
+  const onDrop = useCallback((acceptedFiles: any) => {
+    // Do something with the files
+    setFiles(acceptedFiles.map((file: any) => Object.assign(file, {
+      preview: URL.createObjectURL(file)
+    })))
+  }, [setFiles])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  return (
+    <div {...getRootProps()} style={isDragActive ? {... dzSytles, ...dzActive} : dzSytles}>
+      <input {...getInputProps()} />
+      <Icon name='upload' size='huge' />
+      <Header content='Drop image here' />
+    </div>
+  )
+}
